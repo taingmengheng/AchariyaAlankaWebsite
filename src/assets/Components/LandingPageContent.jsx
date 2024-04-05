b7b51a4603b808b83d0ad6cd3901839ab728c9c0
H.Taingmeng <hengtaingmeng@gmail.com>
2024-04-06 00:09:10 +0700

    Implement: added the fade in effect for doms

--- src/assets/Components/LandingPageContent.jsx
+++ src/assets/Components/LandingPageContent.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
 import { IconButton, Button, Card, Grid } from "@mui/material";
 import EastIcon from "@mui/icons-material/East";
+import FadeInSection from "../Animaton/FadeInSection";
 
 const spanStyle = {
   padding: "20px",
@@ -49,6 +50,32 @@ const properties = {
 };
 
 const LandingPageContent = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      });
+    });
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="slide-container">
       <div>
@@ -74,44 +101,50 @@ const LandingPageContent = () => {
       </div>
 
       <div className="flex items-center justify-around pt-32 pb-4 px-4">
-        <div className="max-w-96">
-          <img src="GroupPicture1.png" alt="GroupPicture" />
-        </div>
-        <div className="w-6/12">
-          <p className="font-elegant text-5xl font-semibold ">
-            Explore The Beauty Of
-          </p>
-          <p className="font-elegant text-5xl font-semibold text-reddish mt-2">
-            Achariya Alanka
-          </p>
-          <div className="my-12">
-            <p className="text-serif text-gray.dark">
-              Indulge in the exquisite allure of Achariya Alanka's jewelry
-              collection. Discover a world where craftsmanship meets elegance,
-              where each piece tells a story of timeless beauty. From dazzling
-              diamonds to lustrous pearls, immerse yourself in a realm of
-              sophistication and style. Explore the artistry of Achariya Alanka
-              and adorn yourself with the epitome of luxury.
-            </p>
+          <div className="max-w-96">
+            <FadeInSection
+              translateType="X"
+              translateValue="-100px"
+            >
+              <img src="GroupPicture1.png" alt="GroupPicture" />
+            </FadeInSection>
           </div>
-          <Button variant="text" color="reddish">
-            <p className="capitalize font-elegant text-lg font-bold">
-              Explore All
-            </p>
-            <EastIcon sx={{ fontSize: "18px", ml: "6px" }} />
-          </Button>
-        </div>
+          <div className="w-6/12">
+            <FadeInSection
+              translateType="X"
+              translateValue="100px"
+            >
+              <p className="font-elegant text-5xl font-semibold ">
+                Explore The Beauty Of
+              </p>
+              <p className="font-elegant text-5xl font-semibold text-reddish mt-2">
+                Achariya Alanka
+              </p>
+              <div className="my-12">
+                <p className="text-serif text-gray.dark">
+                  Indulge in the exquisite allure of Achariya Alanka's jewelry
+                  collection. Discover a world where craftsmanship meets elegance,
+                  where each piece tells a story of timeless beauty. From dazzling
+                  diamonds to lustrous pearls, immerse yourself in a realm of
+                  sophistication and style. Explore the artistry of Achariya Alanka
+                  and adorn yourself with the epitome of luxury.
+                </p>
+              </div>
+              <Button variant="text" color="reddish">
+                <p className="capitalize font-elegant text-lg font-bold">
+                  Explore All
+                </p>
+                <EastIcon sx={{ fontSize: "18px", ml: "6px" }} />
+              </Button>
+            </FadeInSection>
+          </div>
       </div>
 
       <div className="pt-32 pb-4 px-4">
-        <p className="font-elegant text-5xl font-semibold text-center mb-12">
+        <p
+        className="font-elegant text-5xl font-semibold text-center mb-12">
           Explore By Category
         </p>
-        {/* <div className="flex justify-center px-32 mb-6">
-          <p className="text-center">
-            Dive into the world of Achariya Alanka's jewelry through our carefully curated categories. Whether you're seeking classic elegance, modern chic, or statement pieces that command attention, our collection offers something for every style and occasion.
-          </p>
-        </div> */}
         <Grid container spacing={4} sx={{display: 'flex',justifyContent: 'center'}}>
           <Grid item lg={12}>
 
@@ -136,7 +169,8 @@ const LandingPageContent = () => {
             <StyledCategoryItem src="Earing.jpg" title="Earing" />
           </Grid>
         </Grid>
-        <div className="flex justify-center mt-12">
+        <div 
+        className="flex justify-center mt-12">
           <Button variant="text" color="reddish">
             <p className="capitalize font-elegant text-lg font-bold">
               Explore All
@@ -151,24 +185,31 @@ const LandingPageContent = () => {
 
 export default LandingPageContent;
 
-const StyledCategoryItem = ({src, title}) => {
-  return(
-    <div sx={{borderRadius: '8px'}}>
-      <div className="relative overflow-hidden">
-        <img 
-          src={src} 
-          alt={src} 
-          style={{borderRadius: '8px'}} 
-          className="transition-transform duration-500 transform-gpu hover:scale-110 hover:cursor-pointer"
-        />
+const StyledCategoryItem = ({ src, title }) => {
+
+  return (
+    <FadeInSection
+      translateType="Y"
+      translateValue="150px"
+    >
+      <div
+        sx={{ borderRadius: '8px' }}
+      >
+        <div className="relative overflow-hidden">
+          <img
+            src={src}
+            alt={src}
+            style={{ borderRadius: '8px' }}
+            className="transition-transform duration-500 transform-gpu hover:scale-110 hover:cursor-pointer"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <p className="font-elegant text-xl font-semibold mt-2">{title}</p>
+          {/* Assuming EastIcon is defined elsewhere */}
+          <EastIcon sx={{ fontSize: '18px', mt: '8px', color: 'reddish.main' }} />
+        </div>
       </div>
-      <div className="flex items-center justify-between">
-        <p className="font-elegant text-xl font-semibold mt-2">
-          {title}
-        </p>
-        <EastIcon sx={{ fontSize: "18px", mt: '8px', color: 'reddish.main'}} />
-      </div>
-    </div>
-  )
-}
+    </FadeInSection>
+  );
+};
 
