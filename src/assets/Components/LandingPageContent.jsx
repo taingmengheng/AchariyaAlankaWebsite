a12511549063db0799708866c5598bf8b564a4c3
H.Taingmeng <hengtaingmeng@gmail.com>
2024-04-09 23:41:16 +0700

    Style: changed the slide image, added and change the direction of the fadeInSection, Implement: categories var, new arrival section

--- src/assets/Components/LandingPageContent.jsx
+++ src/assets/Components/LandingPageContent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
@@ -6,6 +6,7 @@ import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
 import { IconButton, Button, Card, Grid } from "@mui/material";
 import EastIcon from "@mui/icons-material/East";
 import FadeInSection from "../Animaton/FadeInSection";
+import { AppContext } from "../../Context/AppContext";
 
 const spanStyle = {
   padding: "20px",
@@ -23,18 +24,43 @@ const divStyle = {
 };
 const slideImages = [
   {
+    url: "https://img.freepik.com/free-photo/wedding-bride-holding-pink-flower-bouquet_8353-6094.jpg?t=st=1712672807~exp=1712676407~hmac=d15fc26ec5a025466e570a1066c50eb64c936520125f57d380be6e13928a63fa&w=1060"
+  },
+  {
     url: "https://images.unsplash.com/photo-1621334119886-cbc9e36fcf7c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     url: "https://images.unsplash.com/photo-1569388330338-53ecda03dfa1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
-  {
-    url: "https://images.unsplash.com/photo-1531995811006-35cb42e1a022?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1549048799-bf3b582ee17b?q=80&w=2013&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
 ];
+
+const categories = [
+  {
+    id: 1,
+    title: 'Ring',
+    src: 'Ring (2).jpg'
+  },
+  {
+    id: 2,
+    title: 'Bracelet',
+    src: 'Bracelet.jpg'
+  },
+  {
+    id: 3,
+    title: 'Necklace',
+    src: 'Necklace.jpg'
+  },
+  {
+    id: 4,
+    title: 'Charm',
+    src: 'Charm.jpg'
+  },
+  {
+    id: 5,
+    title: 'Earing',
+    src: 'Earing.jpg'
+  },
+]
 
 const properties = {
   prevArrow: (
@@ -50,6 +76,7 @@ const properties = {
 };
 
 const LandingPageContent = () => {
+  const { jewelry, setJewelry } = useContext(AppContext)
 
   const [isVisible, setIsVisible] = useState(false);
   const elementRef = useRef(null);
@@ -101,83 +128,139 @@ const LandingPageContent = () => {
       </div>
 
       <div className="flex items-center justify-around pt-32 pb-4 px-4">
-          <div className="max-w-96">
-            <FadeInSection
-              translateType="X"
-              translateValue="-100px"
-            >
-              <img src="GroupPicture1.png" alt="GroupPicture" />
-            </FadeInSection>
-          </div>
-          <div className="w-6/12">
-            <FadeInSection
-              translateType="X"
-              translateValue="100px"
-            >
-              <p className="font-elegant text-5xl font-semibold ">
-                Explore The Beauty Of
+        <div className="max-w-96">
+          <FadeInSection translateType="X" translateValue="-100px">
+            <img src="GroupPicture.png" alt="GroupPicture" />
+          </FadeInSection>
+        </div>
+        <div className="w-6/12">
+          <FadeInSection translateType="X" translateValue="100px">
+            <p className="font-elegant text-5xl font-semibold ">
+              Explore The Beauty Of
+            </p>
+            <p className="font-elegant text-5xl font-semibold text-reddish mt-2">
+              Achariya Alanka
+            </p>
+            <div className="my-12">
+              <p className="text-serif text-gray.dark">
+                Indulge in the exquisite allure of Achariya Alanka's jewelry
+                collection. Discover a world where craftsmanship meets elegance,
+                where each piece tells a story of timeless beauty. From dazzling
+                diamonds to lustrous pearls, immerse yourself in a realm of
+                sophistication and style. Explore the artistry of Achariya
+                Alanka and adorn yourself with the epitome of luxury.
               </p>
-              <p className="font-elegant text-5xl font-semibold text-reddish mt-2">
-                Achariya Alanka
+            </div>
+            <Button variant="text" color="reddish">
+              <p className="capitalize font-elegant text-lg font-bold">
+                Explore All
               </p>
-              <div className="my-12">
-                <p className="text-serif text-gray.dark">
-                  Indulge in the exquisite allure of Achariya Alanka's jewelry
-                  collection. Discover a world where craftsmanship meets elegance,
-                  where each piece tells a story of timeless beauty. From dazzling
-                  diamonds to lustrous pearls, immerse yourself in a realm of
-                  sophistication and style. Explore the artistry of Achariya Alanka
-                  and adorn yourself with the epitome of luxury.
-                </p>
-              </div>
-              <Button variant="text" color="reddish">
-                <p className="capitalize font-elegant text-lg font-bold">
-                  Explore All
-                </p>
-                <EastIcon sx={{ fontSize: "18px", ml: "6px" }} />
-              </Button>
-            </FadeInSection>
-          </div>
+              <EastIcon sx={{ fontSize: "18px", ml: "6px" }} />
+            </Button>
+          </FadeInSection>
+        </div>
+      </div>
+
+      <div className="pt-32 pb-32 px-4">
+        <FadeInSection translateType="Y" translateValue="20px">
+          <p className="font-elegant text-5xl font-semibold text-center mb-12">
+            Explore By Category
+          </p>
+        </FadeInSection>
+        <Grid
+          container
+          spacing={4}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          {
+            categories.map((categry, idx) => (
+              <Grid item key={categry.id} lg={2}>
+                <FadeInSection  delay={idx * 50} translateType="Y" translateValue="150px">
+                  <StyledCategoryItem src={categry.src} title={categry.title} />
+                </FadeInSection>
+              </Grid>
+            ))
+          }
+        </Grid>
+        <div className="flex justify-center mt-12">
+          <FadeInSection translateType="Y" translateValue="20px">
+            <Button variant="text" color="reddish">
+              <p className="capitalize font-elegant text-lg font-bold">
+                Explore All
+              </p>
+              <EastIcon sx={{ fontSize: "18px", ml: "6px" }} />
+            </Button>
+          </FadeInSection>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <img src="Banner (2).jpg" alt="Banner.png" width="100%" />
       </div>
 
       <div className="pt-32 pb-4 px-4">
-        <p
-        className="font-elegant text-5xl font-semibold text-center mb-12">
-          Explore By Category
-        </p>
-        <Grid container spacing={4} sx={{display: 'flex',justifyContent: 'center'}}>
-          <Grid item lg={12}>
+        <FadeInSection translateType="Y" translateValue="20px">
+          <p className="font-elegant text-5xl font-semibold text-center mb-12">
+            New Arrivals
+          </p>
+        </FadeInSection>
 
+        <Grid container spacing={4}>
+          <Grid item lg={4}>
+            <FadeInSection translateType="Y" translateValue="150px">
+              <div className="cursor-pointer">
+                <div className="relative overflow-hidden">
+                  <img 
+                  src="18k Jewelry/Necklace/Necklace (1).JPG" 
+                  alt="Golden Necklace" 
+                  className="transition-transform duration-500 transform-gpu hover:scale-110"
+                  />
+                </div>
+                <p className="mt-2 text-md text-gray-dark">
+                  Gold Necklace
+                </p>
+                <div className="flex items-center gap-4">
+                  {/* <p className="font-semibold"v
+                    ${jewelry.discountedPrice}
+                  </p> */}
+                  <p className="font-semibold text-lg">
+                    $999.99
+                  </p>
+                </div>
+              </div>
+            </FadeInSection>
           </Grid>
-          <Grid item lg={2}>
-            <StyledCategoryItem src="Ring (2).jpg" title="Ring" />
-          </Grid>
-
-          <Grid item lg={2}>
-            <StyledCategoryItem src="Bracelet.jpg" title="Bracelet" />
-          </Grid>
-
-          <Grid item lg={2}>
-            <StyledCategoryItem src="Necklace.jpg" title="Necklace" />
-          </Grid>
-
-          <Grid item lg={2}>
-            <StyledCategoryItem src="Charm.jpg" title="Charm" />
-          </Grid>
-
-          <Grid item lg={2}>
-            <StyledCategoryItem src="Earing.jpg" title="Earing" />
+          <Grid container item lg={8} spacing={2}>
+            {
+              jewelry.slice(0, 8).map((jewelry, idx) => (
+                <Grid key={jewelry.id} item lg={3}>
+                  <FadeInSection  delay={idx * 50} translateType="Y" translateValue="150px">
+                    <div className="cursor-pointer">
+                      <div className="relative overflow-hidden">  
+                        <img 
+                        src={jewelry.images[0]} 
+                        alt={jewelry.images[0]}
+                        className="transition-transform duration-500 transform-gpu hover:scale-110"
+                        />  
+                      </div>
+                      <p className="mt-2 text-sm text-gray-dark">
+                        {jewelry.name}
+                      </p>
+                      <div className="flex items-center gap-4">
+                        {/* <p className="font-semibold"v
+                          ${jewelry.discountedPrice}
+                        </p> */}
+                        <p className="font-semibold">
+                          ${jewelry.price}
+                        </p>
+                      </div>
+                    </div>
+                  </FadeInSection>
+                </Grid>
+              ))
+            }
           </Grid>
         </Grid>
-        <div 
-        className="flex justify-center mt-12">
-          <Button variant="text" color="reddish">
-            <p className="capitalize font-elegant text-lg font-bold">
-              Explore All
-            </p>
-            <EastIcon sx={{ fontSize: "18px", ml: "6px" }} />
-          </Button>
-        </div>
       </div>
     </div>
   );
@@ -186,30 +269,27 @@ const LandingPageContent = () => {
 export default LandingPageContent;
 
 const StyledCategoryItem = ({ src, title }) => {
-
   return (
-    <FadeInSection
-      translateType="Y"
-      translateValue="150px"
-    >
-      <div
-        sx={{ borderRadius: '8px' }}
-      >
-        <div className="relative overflow-hidden">
-          <img
-            src={src}
-            alt={src}
-            style={{ borderRadius: '8px' }}
-            className="transition-transform duration-500 transform-gpu hover:scale-110 hover:cursor-pointer"
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <p className="font-elegant text-xl font-semibold mt-2">{title}</p>
-          {/* Assuming EastIcon is defined elsewhere */}
-          <EastIcon sx={{ fontSize: '18px', mt: '8px', color: 'reddish.main' }} />
-        </div>
+    <div>
+      <div className="relative overflow-hidden">
+        <img
+          src={src}
+          alt={src}
+          className="transition-transform duration-500 transform-gpu hover:scale-110 hover:cursor-pointer"
+        />
       </div>
-    </FadeInSection>
+      <div className="flex items-center justify-between px-1 group hover:cursor-pointer">
+        <a className="font-elegant text-xl font-semibold mt-2">{title}</a>
+        <EastIcon
+          className="text-reddish-main text-xl mt-2 cursor-pointer transition-transform duration-300 ease-in-out transform group-hover:translate-x-1"
+          sx={{
+            fontSize: "18px",
+            mt: "8px",
+            color: "reddish.main",
+            cursor: "pointer",
+          }}
+        />
+      </div>
+    </div>
   );
 };
-
